3f0b37776dd84633c0ad8fabc2c8e46c0cd5ee2e
Roman Tarasenko <tarrim80@yandex.ru>
2023-12-12 11:48:35 +0600

    Add menus and sleep_times

--- tg_bot/constants.py
+++ tg_bot/constants.py
@@ -1,7 +1,26 @@
-﻿import os
+﻿import logging
+import os
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
+
+LOGGING_LEVEL = logging.INFO
+
+MENU_ITEMS = {
+    "Частые вопросы": "faq",
+    "Задать по email": "email_question",
+    "Задать вопрос": "custom_question",
+    "Посмотреть котиков": "see_cats",
+    "Взять котика": "adopt_cat",
+    "Попечительство": "patronage",
+    "Пожертвования": "donat",
+}
+
+MENU_LAYOUT = (3, 2, 2)
+
+START_SLEEP = 3
+MENU_SLEEP = 5
