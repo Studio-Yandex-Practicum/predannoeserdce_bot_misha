7b2a06c0a1086bec5c113d8c4e8419988c0ab3a8
Roman Tarasenko <tarrim80@yandex.ru>
2023-12-14 06:52:09 +0600

    Update logger settings

--- tg_bot/settings.py
+++ tg_bot/settings.py
@@ -6,4 +6,4 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=LOGGING_LEVEL,
 )
-logger = logging.getLogger(name="TG_BOT")
+bot_logger = logging.getLogger(name="tg_bot")
