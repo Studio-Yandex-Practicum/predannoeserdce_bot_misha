aac47d4f02199969fd2b5da7f52a00a845bc8501
Roman Tarasenko <tarrim80@yandex.ru>
2023-12-14 06:53:28 +0600

    Update constants

--- tg_bot/constants.py
+++ tg_bot/constants.py
@@ -3,24 +3,46 @@ import os
 
 from dotenv import load_dotenv
 
+from utils import LinkButtonAttributes
+
 load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
+
+TELEGRAM_TOKEN = os.getenv(key="TELEGRAM_TOKEN")
+ADMIN_CHAT_ID = os.getenv(key="ADMIN_CHAT_ID")
 
 LOGGING_LEVEL = logging.INFO
 
-MENU_ITEMS = {
+MENU_ITEMS: dict[str, str] = {
     "Частые вопросы": "faq",
-    "Задать по email": "email_question",
-    "Задать вопрос": "custom_question",
-    "Посмотреть котиков": "see_cats",
-    "Взять котика": "adopt_cat",
-    "Попечительство": "patronage",
-    "Пожертвования": "donat",
+    "Подписаться на рассылку": "subscribe",
 }
 
-MENU_LAYOUT = (3, 2, 2)
+
+LINK_ITEMS: dict[str, LinkButtonAttributes] = {
+    "Попечительство": LinkButtonAttributes(
+        text="Условия попечительства можно посмотреть на сайте",
+        url="https://predannoeserdce.ru/programmy-prijuta/popechitelstvo/",
+    ),
+    "Пожертвование": LinkButtonAttributes(
+        text="Сделать пожертвование можно перейдя по ссылке",
+        url="https://predannoeserdce.ru/help/",
+    ),
+    "Взять котика": LinkButtonAttributes(
+        text="Взять котика домой можно по ссылке",
+        url="https://predannoeserdce.ru/howtohelp/take-home/",
+    ),
+    "Посмотреть котиков": LinkButtonAttributes(
+        text="Посмотреть котиков можно по ссылке",
+        url="https://predannoeserdce.ru/catalog-opeka/",
+    ),
+}
+
+MENU_LAYOUT = 3
 
 START_SLEEP = 3
 MENU_SLEEP = 5
+
+SERVER_API_FAQ_URL = "http://84.201.129.17:8000/api/faq/"
+
+MINUTES_FAQ_UPDATE_INTERVAL = 10
