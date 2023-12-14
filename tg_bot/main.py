dbd5990ab4a01f5b89488112195d231982326faf
Roman Tarasenko <tarrim80@yandex.ru>
2023-12-14 06:49:53 +0600

    Add scheduller

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,5 +1,4 @@
-﻿from constants import TELEGRAM_TOKEN
-from handlers import alert_message, handle_menu_buttons, show_menu_btn, start
+﻿from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
@@ -7,14 +6,35 @@ from telegram.ext import (
     filters,
 )
 
+from constants import MINUTES_FAQ_UPDATE_INTERVAL, TELEGRAM_TOKEN
+from handlers import (
+    alert_message,
+    handle_menu_buttons,
+    show_menu_btn,
+    start,
+    update_faq_list,
+)
+
+scheduller = AsyncIOScheduler()
+scheduller.add_job(
+    func=update_faq_list,
+    trigger="interval",
+    minutes=MINUTES_FAQ_UPDATE_INTERVAL,
+)
+scheduller.start()
+update_faq_list()
+
+
 if __name__ == "__main__":
-    application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+    application = ApplicationBuilder().token(token=TELEGRAM_TOKEN).build()
     handlers = [
-        CommandHandler("start", start),
-        CommandHandler("menu", show_menu_btn),
-        MessageHandler((filters.AUDIO | filters.PHOTO), alert_message),
-        MessageHandler((filters.TEXT), handle_menu_buttons),
+        CommandHandler(command="start", callback=start),
+        CommandHandler(command="menu", callback=show_menu_btn),
+        MessageHandler(
+            filters=(filters.AUDIO | filters.PHOTO), callback=alert_message
+        ),
+        MessageHandler(filters=(filters.TEXT), callback=handle_menu_buttons),
     ]
     for handler in handlers:
-        application.add_handler(handler)
+        application.add_handler(handler=handler)
     application.run_polling()
