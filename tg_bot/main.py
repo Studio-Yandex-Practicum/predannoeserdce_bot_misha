13beff7f3a0386f020e2f0e48f30cb71f9f20296
Roman Tarasenko <tarrim80@yandex.ru>
2023-12-15 04:32:21 +0600

    Move start from handlers.py

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,37 +1,50 @@
 ﻿from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
+    ContextTypes,
     MessageHandler,
     filters,
 )
 
-from constants import MINUTES_FAQ_UPDATE_INTERVAL, TELEGRAM_TOKEN
+from constants import MINUTES_FAQ_UPDATE_INTERVAL, START_SLEEP, TELEGRAM_TOKEN
 from handlers import (
-    alert_message,
+    handle_alert_message,
     handle_menu_buttons,
-    show_menu_btn,
-    start,
-    update_faq_list,
+    handle_show_main_menu,
+    update_faq,
 )
+from message_config import MESSAGES
 
 scheduller = AsyncIOScheduler()
 scheduller.add_job(
-    func=update_faq_list,
+    func=update_faq,
     trigger="interval",
     minutes=MINUTES_FAQ_UPDATE_INTERVAL,
 )
 scheduller.start()
-update_faq_list()
+update_faq()
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Отвечает пользователю на команду /start."""
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=MESSAGES["start"]
+    )
+    await handle_show_main_menu(
+        update=update, context=context, delay=START_SLEEP
+    )
 
 
 if __name__ == "__main__":
     application = ApplicationBuilder().token(token=TELEGRAM_TOKEN).build()
     handlers = [
         CommandHandler(command="start", callback=start),
-        CommandHandler(command="menu", callback=show_menu_btn),
+        CommandHandler(command="menu", callback=handle_show_main_menu),
         MessageHandler(
-            filters=(filters.AUDIO | filters.PHOTO), callback=alert_message
+            filters=(filters.AUDIO | filters.PHOTO),
+            callback=handle_alert_message,
         ),
         MessageHandler(filters=(filters.TEXT), callback=handle_menu_buttons),
     ]
