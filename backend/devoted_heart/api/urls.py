c507407449b8d9c11aa39ae0d354f99c34c735ad
Rolicat <rambleroflife@yandex.ru>
2023-12-17 16:22:06 +0500

    fixing branch

--- backend/devoted_heart/api/urls.py
+++ backend/devoted_heart/api/urls.py
@@ -16,6 +16,7 @@ if ApiEnabled.ENABLE_FAQ:
 if ApiEnabled.ENABLE_CUSTOMER:
     router.register('customer', CustomerVewset)
 
+
 auth_patterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
