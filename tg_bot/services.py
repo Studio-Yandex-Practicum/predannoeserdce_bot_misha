4c59af0180e887ff3e891c4e6917acd6772a5586
Roman Tarasenko <tarrim80@yandex.ru>
2023-12-18 07:34:37 +0600

    Add send custom question functions

---
+++ tg_bot/services.py
@@ -0,0 +1,43 @@
+from telegram import Update
+from telegram.constants import ParseMode
+from telegram.ext import ContextTypes
+
+import keyboards as kb
+from constants import ADMIN_CHAT_ID
+from message_config import ConversationLogMessage, MenuLogMessage
+from settings import bot_logger
+
+
+async def create_question_message(data: dict) -> str:
+    text = (
+        "<b>Вам сообщение!</b>\n\n"
+        f"От кого: {data['user_fullname']}, id:{data['user_id']},\n"
+        f"Email: {data['user_email']},\n"
+        f"Телефон: {data['user_phone']},\n\n"
+        f"Тема: <i>{data['subject']}</i>\n\n"
+        f"Вопрос:\n{data['question']}"
+    )
+    return text
+
+
+async def send_question_tg(
+    update: Update, context: ContextTypes.DEFAULT_TYPE, data: dict
+) -> None:
+    """Отправка вопроса администратору в телеграм."""
+    await context.bot.send_message(
+        chat_id=ADMIN_CHAT_ID,
+        text=await create_question_message(data=data),
+        parse_mode=ParseMode.HTML,
+        reply_markup=await kb.remove_menu(),
+    )
+    bot_logger.info(msg=ConversationLogMessage.SEND_QUESTION % data["user_id"])
+
+
+async def send_question_email(
+    update: Update, context: ContextTypes.DEFAULT_TYPE, data: dict
+) -> None:
+    """Отправка вопроса администратору на email."""
+    await update.message.reply_text(
+        text=MenuLogMessage.STUB_BTN % "`Отправка по email`",
+    )
+    bot_logger.info(msg=ConversationLogMessage.SEND_QUESTION % data["user_id"])
