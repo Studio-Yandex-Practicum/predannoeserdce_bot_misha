b361b155ff1de878e696bd0e7e9935c26327fab5
Alexey Potanin <avpotanin@gmail.com>
2023-12-27 02:12:34 +0300

    test_logout_added:27.12.23

--- backend/devoted_heart/devoted_heart/settings.py
+++ backend/devoted_heart/devoted_heart/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',  # new line
+    'rest_framework_simplejwt',  # new line
     'django_object_actions',
     'djoser',
     'import_export',
