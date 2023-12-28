8f5443df3a8cf0c14e462852c0736cb166138eb5
Павел С. Гришанов <Grishanov@hq.melitta-uv.ru>
2023-12-28 17:10:28 +0300

    add tg test

---
+++ tg_bot/tests/test_ban_button.py
@@ -0,0 +1,21 @@
+import pytest
+
+from keyboards import get_to_ban_button
+from telegram import InlineKeyboardMarkup
+
+test_keyboard = {
+    'text' : 'Заблокировать пользователя',
+    'callback_data' : 'user_to_ban',
+}
+
+
+@pytest.mark.asyncio
+async def test_ban_kb():
+    """Проверяем, что get_to_ban_button, возвращает клавиатуру"""
+
+    keyboards = await get_to_ban_button()
+    button = keyboards.inline_keyboard[0][0]
+
+    assert type(keyboards) == InlineKeyboardMarkup
+    assert button['text'] == test_keyboard['text']
+    assert button['callback_data'] == test_keyboard['callback_data']
